ea7aa00e700d0bfe004e454dfe713183a1602fff
Krithic Kumar <krithickumarub@protonmail.com>
2022-03-25 22:32:00 +0530

    refactor: change class and variable names

--- pystk_gym/common/graphics.py
+++ pystk_gym/common/graphics.py
@@ -5,7 +5,10 @@ from enum import Enum
 import pystk
 
 
-class GraphicType(Enum):
+class GraphicQuality(Enum):
+    """
+    Enum class for all possible graphic qualities.
+    """
 
     HD = (1, pystk.GraphicsConfig.hd)
     SD = (2, pystk.GraphicsConfig.sd)
@@ -18,29 +21,37 @@ class GraphicType(Enum):
         obj._obj_ref = obj_ref
         return obj
 
-    def get_obj(self):
+    def get_obj(self) -> pystk.GraphicsConfig:
         return self._obj_ref()
 
 
 class GraphicConfig:
-    def __init__(self, width: int, height: int, graphic_type: GraphicType) -> None:
+    def __init__(self, width: int, height: int, graphic_quality: GraphicQuality) -> None:
+        """
+        :param width: screen width
+        :param height: screen height
+        :param graphic_quality: graphic quality
+        """
         self.width = width
         self.height = height
-        self.graphic_type = graphic_type
+        self.graphic_quality = graphic_quality
 
     def _get_graphic_config(self) -> pystk.GraphicConfig:
-        config = self.graphic_type.get_obj()
+        """Internal method to get a pystk.GraphicConfig object."""
+        config = self.graphic_quality.get_obj()
         config.screen_width = self.width
         config.screen_height = self.height
         return config
 
     @staticmethod
     def default_config() -> pystk.GraphicConfig:
-        return GraphicConfig(600, 400, GraphicType.HD)._get_graphic_config()
+        """Default graphic config."""
+        return GraphicConfig(600, 400, GraphicQuality.HD)._get_graphic_config()
 
     @classmethod
     def get_graphic_config(
-        cls, width: int, height: int, graphic_type: GraphicType
+        cls, width: int, height: int, graphic_type: GraphicQuality
     ) -> pystk.GraphicConfig:
+        """Get pystk.GraphicConfig object using the parameters."""
         config = cls(width, height, graphic_type)
         return config._get_graphic_config()
