e6186e69246316d153085233bcafa69dc91d83cb
Krithic Kumar <krithickumarub@protonmail.com>
2024-05-04 23:58:32 +0530

    refactor: migrate to setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pystk_gym",
+    version="1.0.0",
+    author="Krithic Kumar",
+    author_email="temp.terrafly@slmail.me",
+    description="",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/notjedi/pystk-gym",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+    python_requires=">=3.9",
+    install_requires=[
+        "numpy",
+        "sympy",
+        "PyQt5",
+        "gymnasium",
+        "matplotlib",
+        "PySuperTuxKart",
+        "pettingzoo[all]",
+    ],
+    extras_require={
+        "dev": ["mypy", "black", "isort", "flake8", "pytest", "pre-commit"]
+    },
+)
