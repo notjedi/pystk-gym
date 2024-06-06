fix & perf: choosing wrong path_node when more than one path is available & ditch sympy and move to pure numpy for distance calculation

pystk_gym/common/kart.py
@@ -3,9 +3,9 @@ from typing import Any, Dict, List
 import numpy as np
 import numpy.typing as npt
 import pystk
-from sympy import Line3D, Point3D
 
 from .info import Info
+from .race import LineType
 
 
 class Kart:
@@ -14,7 +14,7 @@ class Kart:
         kart: pystk.Kart,
         is_reverse: bool,
         path_width: npt.NDArray[np.float32],
-        path_lines: List[Line3D],
+        path_lines: List[LineType],
         path_distance: npt.NDArray[np.float32],
         return_info: bool = True,
     ):
@@ -33,19 +33,42 @@ class Kart:
         self.out_of_track_count = 0
         self._node_idx = 0
 
+    @staticmethod
+    def get_dist_bw_line_and_point(
+        line_points: np.ndarray[np.ndarray, np.dtype[np.float32]],
+        point: npt.NDArray[np.float32],
+    ) -> float:
+        # https://stackoverflow.com/questions/39840030/distance-between-point-and-a-line-from-two-points
+        dist = np.linalg.norm(
+            np.cross(line_points[1] - line_points[0], line_points[0] - point)
+        ) / np.linalg.norm(line_points[1] - line_points[0])
+        return dist.item()
+
     def _update_node_idx(self):
         dist_down_track = (
             0
             if self.is_reverse and self.kart.overall_distance <= 0
             else self.kart.distance_down_track
         )
-        path_dist = self.path_distance[self._node_idx]
-        while not path_dist[0] <= dist_down_track <= path_dist[1]:
-            if dist_down_track < path_dist[0]:
-                self._node_idx -= 1
-            elif dist_down_track > path_dist[1]:
-                self._node_idx += 1
-            path_dist = self.path_distance[self._node_idx]
+        min_path_dist_check = np.array(
+            [
+                path_dist[0] <= dist_down_track <= path_dist[1]
+                for path_dist in self.path_distance
+            ]
+        )
+        num_true_vals = np.sum(min_path_dist_check)
+        idxs = np.flatnonzero(min_path_dist_check)
+        if num_true_vals > 1:
+            kart_loc = self.kart.location
+            dist_from_centers = np.array(
+                [
+                    Kart.get_dist_bw_line_and_point(self.path_lines[idx], kart_loc)
+                    for idx in idxs
+                ]
+            )
+            self._node_idx = idxs[np.argmin(dist_from_centers).item()]
+        else:
+            self._node_idx = idxs.item()
 
     def _get_jumping(self) -> bool:
         return self.kart.jumping
@@ -62,13 +85,17 @@ class Kart:
     def _get_overall_distance(self) -> int:
         return max(0, self.kart.overall_distance)
 
+    def _get_distance_down_track(self) -> int:
+        return self.kart.distance_down_track
+
     def _get_location(self) -> List[int]:
         return self.kart.location
 
     def _get_kart_dist_from_center(self) -> float:
-        location = self.kart.location
+        kart_loc = np.array(self.kart.location, dtype=np.float32)
         path_node = self.path_lines[self._node_idx]
-        return float(path_node.distance(Point3D(location)).evalf())  # type: ignore
+        dist = Kart.get_dist_bw_line_and_point(path_node, kart_loc)
+        return dist
 
     def _get_is_inside_track(self) -> bool:
         curr_path_width = self.path_width[self._node_idx][0]
@@ -93,12 +120,13 @@ class Kart:
         info[Info.ATTACHMENT] = self._get_attachment()
         info[Info.FINISH_TIME] = self._get_finish_time()
         info[Info.IS_INSIDE_TRACK] = self._get_is_inside_track()
-        info[Info.OVERALL_DISTANCE] = self._get_overall_distance()
+        info[Info.DISTANCE_DOWN_TRACK] = self._get_distance_down_track()
 
         # info based on _prev_info
         if self._prev_info:
             delta_dist = (
-                info[Info.OVERALL_DISTANCE] - self._prev_info[Info.OVERALL_DISTANCE]
+                info[Info.DISTANCE_DOWN_TRACK]
+                - self._prev_info[Info.DISTANCE_DOWN_TRACK]
             )
             info[Info.DELTA_DIST] = delta_dist
             if delta_dist < 0:

pystk_gym/common/race.py
@@ -6,9 +6,9 @@ from typing import Any, Dict, Iterable, List, Optional, Union
 import numpy as np
 import numpy.typing as npt
 import pystk
-from sympy import Line3D
 
 ObsType = np.ndarray[np.ndarray, np.dtype[np.uint8]]
+LineType = np.ndarray[np.ndarray, np.dtype[np.float32]]
 
 
 class RaceConfig:
@@ -172,21 +172,20 @@ class Race:
         info["difficulty"] = self.config.difficulty
         return info
 
-    def get_path_lines(self) -> List[Line3D]:
-        lines = [Line3D(*node) for node in self.track.path_nodes]
+    def get_path_lines(self) -> List[LineType]:
         if self.config.reverse:
-            return lines[::-1]
-        return lines
+            return self.track.path_nodes[::-1]
+        return self.track.path_nodes
 
     def get_path_width(self) -> npt.NDArray[np.float32]:
         if self.config.reverse:
-            return np.array(self.track.path_width[::-1])
-        return np.array(self.track.path_width)
+            return self.track.path_width[::-1]
+        return self.track.path_width
 
     def get_path_distance(self) -> npt.NDArray[np.float32]:
         if self.config.reverse:
-            return np.array(self.track.path_distance[::-1])
-        return np.array(self.track.path_distance)
+            return self.track.path_distance[::-1]
+        return self.track.path_distance
 
     @functools.lru_cache(maxsize=None)
     def get_controlled_kart_mask(self) -> List[bool]:
@@ -209,9 +208,11 @@ class Race:
     def get_controlled_karts(self) -> List[pystk.Kart]:
         return list(np.array(self.get_all_karts())[self.get_controlled_kart_mask()])
 
-    def get_nitro_locs(self) -> List[List[int]]:
+    def get_nitro_locs(self) -> List[npt.NDArray[np.float32]]:
         return [
-            item.location for item in self.state.items if item in RaceConfig.NITRO_TYPE
+            np.array(item.location)
+            for item in self.state.items
+            if item.type in RaceConfig.NITRO_TYPE
         ]
 
     def get_all_kart_rankings(self) -> Dict[int, int]:

pystk_gym/envs/race_env.py
@@ -69,6 +69,7 @@ class RaceEnv(ParallelEnv):
             3,
         )
         self._make_karts(return_info)
+        self.nitro_locs = self.race.get_nitro_locs()
 
         self.env_viewer: Optional[EnvViewer] = None
         if render_mode in ("human", "agent"):
@@ -108,13 +109,13 @@ class RaceEnv(ParallelEnv):
         }
 
     def _update_info_dict_with_race_info(self, infos: Dict[AgentId, Dict[Info, Any]]):
-        nitro_locs = self.race.get_nitro_locs()
         all_kart_rankings = self.race.get_all_kart_rankings()
         for info, kart in zip(infos.values(), self.get_controlled_karts()):
             info[Info.RANK] = all_kart_rankings[kart.id]
             kart_loc = np.array(info[Info.LOCATION])
             info[Info.NITRO] = any(
-                np.sqrt(np.sum(kart_loc - nitro_loc)) <= 1 for nitro_loc in nitro_locs
+                np.sqrt(np.sum(np.square(kart_loc - nitro_loc))) <= 2
+                for nitro_loc in self.nitro_locs
             )
 
     def _get_reward(
@@ -130,9 +131,9 @@ class RaceEnv(ParallelEnv):
     def _terminal(self, infos: Dict[AgentId, dict]) -> Dict[AgentId, bool]:
         step_limit_reached = self.steps > self.max_step_cnt
         return {
-            agent_id: info[Info.OUT_OF_TRACK_COUNT] > 25
+            agent_id: info[Info.OUT_OF_TRACK_COUNT] > 50
             or info[Info.BACKWARD_COUNT] > 50
-            or info[Info.NO_MOVEMENT_COUNT] > 25
+            or info[Info.NO_MOVEMENT_COUNT] > 50
             or info[Info.DONE]
             or step_limit_reached
             for agent_id, info in infos.items()
@@ -188,7 +189,6 @@ class RaceEnv(ParallelEnv):
             for kart in self.get_controlled_karts()
             if not (terminals[kart.id] or truncated[kart.id])
         ]
-        self.render(self.render_mode)
         return obs, rewards, terminals, truncated, infos
 
     def render(